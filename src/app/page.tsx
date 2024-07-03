459e6a176985cbbf4cf77b47718d6cfc7c09ccb9
KenMwaura1 <kemwaura@gmail.com>
2024-07-04 02:32:08 +0300

    feat: Add API data lookup functionality
    This commit adds functionality to fetch data from a Django API and display it on the page. It introduces a new button that triggers the API call and updates the state with the response data. The fetched data is then displayed in a fixed positioned paragraph element.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [data, setData] = useState({});
+
+  async function getDjangoAPIData() {
+    const response = await fetch('http://127.0.0.1:8001/api/hello');
+    const responseData = await response.json();
+    // console.log(data);
+    setData(responseData);
+  }
+  async function handleClick(){
+    await getDjangoAPIData();
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button className="mb-3 text-2xl font-semibold" onClick={handleClick}> Lookup Data from API
+      </button>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          {JSON.stringify(data)}
+        </p>
+      </div>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
